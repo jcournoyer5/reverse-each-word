1d0dcbe41a9ae334706089b8e94e4e27fa359bf4
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-17 14:52:35 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
+     array << sentence.split
     #sentence.split.map {|word| word.reverse}.join(" ")
-    sentence.collect.map {|word| word.reverse}.join(" ")
+    sentence.collect {|word| word.reverse}.join(" ")
 
 end
 
